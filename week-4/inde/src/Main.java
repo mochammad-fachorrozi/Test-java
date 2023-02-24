35b62bd990542c799abc417770b8f76449026ec9
mochammad-fachorrozi <mochammadfachorrozi98@gmail.com>
2023-02-24 23:34:20 +0700

    edit file inde

--- week-4/inde/src/Main.java
+++ week-4/inde/src/Main.java
@@ -1,24 +1,61 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.regex.MatchResult;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+        // List emails = new ArrayList();
+        ArrayList<String> emails = new ArrayList<String>();
+        emails.add("user@domain.com");
+        emails.add("user#domain.co.id");
 
-    public static void main(String []args) {
-        // Validate if a string is a date
-        boolean isMatch = Pattern.compile("^[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{4}$")
-                .matcher("12/12/2022")
-                .find();
-        System.out.println(isMatch); // prints true
+        String regexEmail = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern patternEmail = Pattern.compile(regexEmail);
 
-        String email = Pattern.compile(\"\\\\b[\\\\w.%-]+@[-.\\\\w]+\\\\.[A-Za-z]{2,4}\\\\b\");
-        String tanggal = Pattern.compile("^[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{4}$")
-        // Extract date from a string
-//        String[] matches = Pattern.compile("[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{4}")
-//                .matcher("I\'m on vacation from 1/18/2021 till 1/29/2021")
-//                .results()
-//                .map(MatchResult::group)
-//                .toArray(String[]::new);
-//        System.out.println(Arrays.toString(matches)); // prints [1/18/2021, 1/29/2021]
+        System.out.println("Data email yang valid atau tidak :");
+        for(String email : emails){
+            Matcher matcher = patternEmail.matcher(email);
+            System.out.println(email +" : "+ matcher.matches());
+        }
+
+        //format dd-MM-yyyy
+        ArrayList<String> dates = new ArrayList<String>();
+        dates.add("01-12-2000");
+        dates.add("01-13-2000");
+
+        String regexDate = "(0[1-9]|1[0-9]|2[0-9]|3[0-1]|[1-9])-(0[1-9]|1[0-2]|[1-9])-([0-9]{4})";
+        Pattern patternDate = Pattern.compile(regexDate);
+
+        System.out.println("");
+        System.out.println("Data tanggal dengan format dd-MM-yyyy : ");
+        for(String date : dates){
+            Matcher matcher = patternDate.matcher(date);
+            System.out.println(date +" : "+ matcher.matches());
+        }
+
+        // format ip addres
+        ArrayList<String> ips = new ArrayList<String>();
+        ips.add("192.168.0.1");
+        ips.add("192.168.0.256");
+
+        // Regex for digit from 0 to 255.
+        String zeroTo255 = "(\\d{1,2}|(0|1)\\"
+                + "d{2}|2[0-4]\\d|25[0-5])";
+        String regexIp
+                = zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255;
+        Pattern patternIp = Pattern.compile(regexIp);
+
+        System.out.println("");
+        System.out.println("Data Ip Address  : ");
+
+        for(String ip : ips){
+            Matcher matcher = patternIp.matcher(ip);
+            System.out.println(ip +" : "+ matcher.matches());
+        }
     }
 }
